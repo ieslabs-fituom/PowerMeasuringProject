fb9ebd5be2d76c335a9faf1164ece9d693c37791
Sankha Bimsara <sankha.b21@gmail.com>
2023-10-17 14:21:37 +0530

    dependencies added to web

---
+++ Web App/app/_app.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <Component {...pageProps} />
+    )
+}
---
+++ Web App/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <button className='btn btn-primary'>
+      asdjha
+    </button>
+  )
+}
