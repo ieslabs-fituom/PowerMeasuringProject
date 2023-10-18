b5f8503343e7edea0222d2f2e947719759f4440c
imnthiyanga <ithiyanga@gmail.com>
2023-10-18 15:54:25 +0530

    navbar_creating

--- WebApp/pages/deviceSettings.js
+++ WebApp/pages/deviceSettings.js
@@ -1,10 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import { useState, useEffect } from 'react';
-import { Router, useRouter } from 'next/router';
+import "bootstrap/dist/css/bootstrap.css";
+import navBarStyles from "../public/styles/navbar.module.css";
+import { useState, useEffect } from "react";
+import { Router, useRouter } from "next/router";
 
 export default function DeviceSettings() {
-    console.log("deviceSettings");
-    return(
-        <button>askdjh</button>
-    )
+  console.log("deviceSettings");
+  return (
+    <div className={`${navBarStyles.navbar}`}>
+        <div><img src="../public/" alt="BigCo Inc. logo"/></div>
+    </div>
+  ) 
 }
