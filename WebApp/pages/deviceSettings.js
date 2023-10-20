d5eeb6fc4bfae0e9339ff1ad8e03abc1ab7bc052
imnthiyanga <ithiyanga@gmail.com>
2023-10-20 14:07:20 +0530

    Settings Page

--- WebApp/pages/deviceSettings.js
+++ WebApp/pages/deviceSettings.js
@@ -1,13 +1,29 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Image from 'next/image';
 import navBarStyles from "../public/styles/navbar.module.css";
+import SettingsStyles from "../public/styles/deviceSettings.module.css";
+
 import { useState, useEffect } from "react";
 import { Router, useRouter } from "next/router";
 
 export default function DeviceSettings() {
   console.log("deviceSettings");
   return (
+    <><div className={`${SettingsStyles.background}`}>
     <div className={`${navBarStyles.navbar}`}>
-        <div><img src="../public/" alt="BigCo Inc. logo"/></div>
+      <div className={`${navBarStyles.logo}`}>
+        <Image
+          src="/icons/Hospital_Logo.png"
+          Max width={120}
+          height={80}
+          alt="Picture of the author" />
+      </div>
     </div>
-  ) 
+    <div className={`${SettingsStyles.form}`}>
+      <div className={``}>
+        
+      </div>
+    </div>
+      </div></>
+  );
 }
