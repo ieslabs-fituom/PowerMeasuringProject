64e53c83fab66aba9a349d0bcb2ef99dc1f2db93
Sankha Bimsara <sankha.b21@gmail.com>
2023-10-23 12:11:08 +0530

    allDevices partially done

--- WebApp/pages/components/button.js
+++ WebApp/pages/components/button.js
@@ -2,17 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "bootstrap/dist/css/bootstrap.css";
 import Image from 'next/image';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle, faFilter, faPlus, faSave } from "@fortawesome/free-solid-svg-icons";
 
-const Button = ({ text, disabled, onClick, icon, ms, mt, me, mb }) => {
+const Button = ({ text, disabled, onClick, icon, iconColor, ms, mt, me, mb, bgcolor, width }) => {
+    const styles = {
+        width: width,
+    }
     return (
-        <button disabled={disabled} onClick={onClick} className={`btn border btn-primary rounded-2 d-flex justify-content-center align-items-center ${mt} ${ms} ${mb} ${me}`}>
-            <span style={{fontSize:13}}>{text}</span>
-            <Image
+        <button disabled={disabled} onClick={onClick} className={`btn ${bgcolor} rounded-2 d-flex justify-content-center align-items-center ${mt} ${ms} ${mb} ${me}`}
+            style={styles}>
+            <span style={{ fontSize: 13 }}>{text}</span>
+            {/* <Image
                 src={icon}
                 width={14}
                 height={14}
                 alt="Logo"
-                className={`ms-3`} />
+                className={`ms-3`} /> */}
+            <FontAwesomeIcon icon={icon} className={`${iconColor} align-self-center ms-3`} />
         </button>
     );
 };
@@ -28,6 +35,8 @@ Button.defaultProps = {
     mb: 0,
     ms: 0,
     me: 0,
+    width: 'auto',
+    iconColor: 'text-dark',
 };
 
 export default Button;
--- WebApp/pages/deviceSettings.js
+++ WebApp/pages/deviceSettings.js
@@ -1,4 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload, faSave } from "@fortawesome/free-solid-svg-icons";
 import { Montserrat } from 'next/font/google';
 import Image from 'next/image';
 import navBarStyles from "../public/styles/navbar.module.css";
@@ -22,7 +24,7 @@ export default function DeviceSettings() {
             height={120}
             alt="Logo"
             className={`rounded-circle object-fit-cover`} />
-          <ButtonComponent text="Select Image" disabled={false} onClick={() => { }} icon={'/icons/upload.png'} mt={'mt-2'} />
+          <ButtonComponent text="Select Image" disabled={false} onClick={() => { }} icon={faUpload} mt={'mt-2'} bgcolor={'btn-primary'} iconColor={'text-white'} />
           <div className={``}>
             <p className={`${SettingsStyles.topic} text-center fw-bold mt-4`}>Add New Device</p>
           </div>
@@ -55,7 +57,7 @@ export default function DeviceSettings() {
               </div>
             </div>
           </div>
-          <ButtonComponent text="Add Device" disabled={false} onClick={() => { }} icon={'/icons/plus-white.png'} mt={'mt-5'} />
+          <ButtonComponent text="Add Device" disabled={false} onClick={() => { }} icon={faSave} mt={'mt-5'} bgcolor={'btn-primary'} iconColor={'text-white'}/>
 
         </div>
       </div>
