d0ac5ea0bd2239c762911c00d8208daa3d18d629
Sankha Bimsara <sankha.b21@gmail.com>
2023-11-14 01:16:37 +0530

    deviceDetails table done without pagination

--- WebApp/pages/deviceDetails.js
+++ WebApp/pages/deviceDetails.js
@@ -19,7 +19,7 @@ import axios from 'axios';
 import { SharedContext } from '../contexts/sharedContext';
 
 
-export default function AllDevices({device}) {
+export default function AllDevices({ device }) {
 
     const router = useRouter();
     const { userId, email, setUserId, setEmail } = useContext(SharedContext);
@@ -27,31 +27,30 @@ export default function AllDevices({device}) {
     const [deviceName, setDeviceName] = useState('');
     const [deviceType, setDeviceType] = useState('');
     const [deviceLocation, setDeviceLocation] = useState('');
-    const [deviceThreshold, setDeviceThreshold] = useState(0);
     const [usageData, setUsageData] = useState([
         {
-            id: 1,
+            record_id: 1,
             date: '2023-01-10',
             startTime: '10:00:00',
             endTime: '10:00:00',
             duration: '01 Hour'
         },
         {
-            id: 2,
+            record_id: 2,
             date: '2023-01-10',
             startTime: '10:00:00',
             endTime: '10:00:00',
             duration: '01 Hour'
         },
         {
-            id: 3,
+            record_id: 3,
             date: '2023-01-10',
             startTime: '10:00:00',
             endTime: '10:00:00',
             duration: '01 Hour'
         },
         {
-            id: 4,
+            record_id: 4,
             date: '2023-01-11',
             startTime: '10:00:00',
             endTime: '10:00:00',
@@ -76,10 +75,10 @@ export default function AllDevices({device}) {
         setDeviceName(deviceDetails.device_name);
         setDeviceType(deviceDetails.device_type);
         setDeviceLocation(deviceDetails.device_location);
-        setDeviceThreshold(deviceDetails.device_threshold);
 
         getDeviceTypeDetails(deviceDetails.device_type);
         getDeviceLocationDetails(deviceDetails.device_location);
+        getDeviceUsageDetails(device);
     }
 
     // Function to load device type details using the device type id
@@ -102,6 +101,65 @@ export default function AllDevices({device}) {
         console.log(res.data.result);
         const deviceLocationDetails = res.data.result[0];
         setDeviceLocation(deviceLocationDetails.location_name);
+    }
+
+    // Function to load device usage details using the device id
+    const getDeviceUsageDetails = async (device) => {
+        const res = await axios.post('/api/getRecords', {
+            deviceId: device
+        });
+        console.log(res.data.result);
+        processUsageData(res.data.result);
+    }
+
+    // Function to process the device usage data
+    const processUsageData = (usageData) => {
+        usageData.forEach(record => {
+            const startTime = new Date(record.start_time);
+            const endTime = new Date(record.end_time);
+
+            // Calculate the time difference in milliseconds
+            const timeDifference = endTime - startTime;
+
+            // Calculate hours and minutes
+            const hours = Math.floor(timeDifference / 3600000); // 1 hour = 3600000 milliseconds
+            const minutes = Math.floor((timeDifference % 3600000) / 60000); // 1 minute = 60000 milliseconds
+
+            // Format the duration
+            const formattedDuration = `${hours} Hours ${minutes} minutes`;
+
+            console.log(formattedDuration, startTime, endTime); // Output: "1 Hours 0 minutes"
+
+            record.duration = formattedDuration;
+
+            // Start time and end time formating
+            // Function to format a date as DD-MM-YY HH:MM:SS
+            const formatDate = (date) => {
+                const day = String(date.getDate()).padStart(2, "0");
+                const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
+                const year = String(date.getFullYear());
+                const hours = String(date.getHours()).padStart(2, "0");
+                const minutes = String(date.getMinutes()).padStart(2, "0");
+                const seconds = String(date.getSeconds()).padStart(2, "0");
+
+                return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+            };
+
+            // Format start_time and end_time
+            const formattedStartTime = formatDate(startTime);
+            const formattedEndTime = formatDate(endTime);
+
+            // Extract the date of start_time as DD-MM-YY
+            const startDate = formatDate(startTime).split(" ")[0];
+
+            record.startTime = formattedStartTime;
+            record.endTime = formattedEndTime;
+            record.date = startDate;
+        });
+
+        setUsageData(usageData);
+
+
     }
 
     // This is the wrapper around the circular progress bar
@@ -146,7 +204,7 @@ export default function AllDevices({device}) {
             paddingBottom: 15,
         }
         return (
-            <tr className={`text-center`} key={rowData.id}>
+            <tr className={`text-center`} key={rowData.record_id}>
                 <td style={cellStyle}>{rowData.date}</td>
                 <td style={cellStyle}>{rowData.startTime}</td>
                 <td style={cellStyle}>{rowData.endTime}</td>
