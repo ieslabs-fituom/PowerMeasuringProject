1871f3ac0662dbb0bb99f2fd22d5a009d7d10da3
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2023-12-22 01:47:05 +0530

    added signin

--- WebApp/pages/signin.js
+++ WebApp/pages/signin.js
@@ -8,20 +8,47 @@ import ButtonComponent from "./components/button";
 import SettingsStyles from "../public/styles/deviceSettings.module.css";
 import SigninStyles from "../public/styles/signin.module.css";
 import { useState, useEffect, useContext } from "react";
-import { Router, useRouter } from "next/router";
-
+import Router from "next/router";
 import HeaderComponent from "./components/header";
+import {app} from '../firebase';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+
+
+
 
 export default function AllDevices() {
+    const auth = getAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState("");
 
+    const handleSignin = () =>{
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setError('Login Success!');
+                Router.push('/allDevices');
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                if (errorCode === 'auth/wrong-password') {
+                    setError('Wrong Credentials entered.');
+                } else if (errorCode === 'auth/user-not-found') {
+                    setError('User not found.');
+                } else if (errorCode === 'auth/invalid-credential') {
+                    setError('Invalid credentials provided.');
+                } else {
+                    setError("Something went wrong! " + errorMessage);
+                }
+            });
+
+    }
 
     return (
         <>
             <div className={`${SettingsStyles.background}`}>
                 <HeaderComponent />
-                
-
-            {/* create a login form to sign up user using bootstrap */}
 
             <div className={`${SigninStyles.signinContainer}`}>
                 <div className={`${SigninStyles.signinForm}`}>
@@ -29,24 +56,37 @@ export default function AllDevices() {
                         <h1 className={`${SigninStyles.signinFormHeaderTitle}`}>Sign In</h1>
                     </div>
                     <div className={`${SigninStyles.signinFormBody}`}>
-                        <form>
+                        <div className={`${SigninStyles.signinFormError}`}>
+                            <p>{error}</p>
+                        </div>
+                        <div>
                             <div className={`${SigninStyles.signinFormBodyInput}`}>
                                 <label className={`${SigninStyles.signinFormBodyInputLabel}`}>Email</label>
-                                <input type="email" className={`${SigninStyles.signinFormBodyInputField}`} placeholder="Enter your email" />
+                                <input type="email" className={`${SigninStyles.signinFormBodyInputField}`} placeholder="Enter your email" onChange={(e) => {setEmail(e.target.value)}} />
                             </div>
                             <div className={`${SigninStyles.signinFormBodyInput}`}>
                                 <label className={`${SigninStyles.signinFormBodyInputLabel}`}>Password</label>
-                                <input type="password" className={`${SigninStyles.signinFormBodyInputField}`} placeholder="Enter your password" />
+                                <input type="password" className={`${SigninStyles.signinFormBodyInputField}`} placeholder="Enter your password" onChange={(e) => {setPassword(e.target.value)}}/>
                             </div>
                             <div className={`${SigninStyles.signinFormBodyInput}`}>
-                            <ButtonComponent text="Sign In" onClick={() => { }} icon={faUser} mt={'mt-1'} mb={'mb-1'} ms={'ms-1'} me={'me-1'} bgcolor={'btn-primary'} width={'auto'}  />
+                            <ButtonComponent text="Sign In" onClick={handleSignin} icon={faUser} mt={'mt-1'} mb={'mb-1'} ms={'ms-1'} me={'me-1'} bgcolor={'btn-primary'} width={'auto'}  />
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
                 </div>
                 
             </div>
+
+            <div className={`${SigninStyles.footer}`}>
+                <p className={`text-center`}>© 2024 IoT and Embedded Systems Labs - Faculty of Information Technology. University of Moratuwa. </p>
+
+            </div>
+
+
+
+
+
         </>
     )
 }
