3cae2d4de5410043561add642a60bd2f886332d5
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2023-12-24 16:26:40 +0530

    added request function to firmware

--- CurrentProject/CurrentProject.ino
+++ CurrentProject/CurrentProject.ino
@@ -1,12 +1,55 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+
+const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+const char* server = "http://localhost/DB-api.php";
+String apikey = "lukepramo221#";
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(2, OUTPUT);
+  WIFI.begin(ssid,password);
+  Serial.println("Connecting to WIFI");
+  while(WIFI.status() != WL_CONNECTED){
+    Serial.println(".");
+    delay(500);
+  }
+  Serial.println("Connection success to : " + WIFI.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (Serial.readString() == "!"){
     digitalWrite(2, HIGH);
   }
+  //call the send_request function as per the conditions
+
+  void send_request(String device_id, String start_time, String end_time){
+    if(WiFi.status()== WL_CONNECTED){
+    WiFiClientSecure *client = new WiFiClientSecure;
+    client->setInsecure();
+    HTTPClient https;
+    https.begin(*client, server);
+    https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String Request = "api_key=" + apikey + "&device_id=" + sensorName + "&start_time=" + start_timen + "&end_time" + end_time;
+    Serial.println(Request);
+    int httpResponseCode = https.POST(Request);
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    https.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  }
+
+  
+}
 }
