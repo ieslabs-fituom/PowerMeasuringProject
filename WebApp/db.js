e5fda988a6d646f5f3aa8a426b8dfe1a8767eafb
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-01-19 17:58:22 +0530

    added DB info

--- WebApp/db.js
+++ WebApp/db.js
@@ -2,11 +2,11 @@ import mysql from 'serverless-mysql';
 
 const db = mysql({
   config: {
-    host: 'localhost',
-    port: 3306,
-    database: 'power_project',
-    user: 'root',
-    password: ''
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
   }
 });
 
